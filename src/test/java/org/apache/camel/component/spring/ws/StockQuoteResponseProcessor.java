626f8d996ba7930fa34fe20818f44be79330ffcd
Richard Kettelerij <richardkettelerij@gmail.com>
2010-10-03 15:07:46 +0200

    Switch to Java 1.5

--- src/test/java/org/apache/camel/component/spring/ws/StockQuoteResponseProcessor.java
+++ src/test/java/org/apache/camel/component/spring/ws/StockQuoteResponseProcessor.java
@@ -7,15 +7,19 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Document;
 
 /**
  * Generates static response on StockQuote webservice requests
  */
 public class StockQuoteResponseProcessor implements Processor {
+	
+    private static final Log LOG = LogFactory.getLog(StockQuoteResponseProcessor.class);
 
-	@Override
 	public void process(Exchange exchange) throws Exception {
+		LOG.info("Crafting standard response in StockQuoteResponseProcessor");
 		File file = new File("src/test/resources/stockquote-response.xml");
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
