6093258d25823b38a5e7e86b295b5317f2bee13c
Richard Kettelerij <richardkettelerij@gmail.com>
2010-10-03 15:07:11 +0200

    switch to Java 1.5

--- src/main/java/org/apache/camel/component/spring/ws/bean/CamelEndpointDispatcher.java
+++ src/main/java/org/apache/camel/component/spring/ws/bean/CamelEndpointDispatcher.java
@@ -43,7 +43,6 @@ public class CamelEndpointDispatcher implements MessageEndpoint {
 
 	private MessageEndpoint consumerMessageEndpoint;
 
-	@Override
 	public void invoke(MessageContext messageContext) throws Exception {
 		Assert.notNull(consumerMessageEndpoint);
 		consumerMessageEndpoint.invoke(messageContext);
